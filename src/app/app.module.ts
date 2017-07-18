5841ce03b01f1400cdcc76016514cc3e71ec4560
Arjun Rao <contact@raoarjun.com>
2017-07-18 12:36:28 -0700

    Added Todo List and Sign In components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: TodoListComponent
+    },
+    {
+        path: 'sign-in',
+        component: SignInComponent
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
